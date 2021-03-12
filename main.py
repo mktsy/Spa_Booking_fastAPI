db74ead15c9d758fdb309fead04350c4f805227a
mktsy <64907415+mktsy@users.noreply.github.com>
2021-03-12 14:11:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
 
-# define an entry point for tunning the application
+# define an entry point for tunning the application.
 if __name__ == "__main__":
     uvicorn.run("server.app:app", host="0.0.0.0", reload=True)
